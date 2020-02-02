e970778012f073ada744aa5e5473b6dcd09f7598
Anton Zavyalov <megadeathlightsaber@gmail.com>
2020-02-02 20:33:06 +0600

    fill documentation page of application

---
+++ src/components/Documentation.svelte
@@ -0,0 +1,22 @@
+<style>
+    .documentation {
+        width: 100%;
+        padding: 30vh 20%;
+        text-align: center;
+    }
+</style>
+
+<div class="documentation">
+    <p>
+        <b>darkstalker</b> draws a friends graph for the VK user with specified identifier.
+    </p>
+    <p>
+        You can navigate between users by clicking on them in the graph.
+    </p>
+    <p>
+        made by <a href="https://github.com/cochegarka/" target="_blank">cochegarka inc.</a>
+        with <a href="https://golang.org" target="_blank">Go</a>,
+        <a href="https://gokit.io/" target="_blank">Go Kit</a>
+        and <a href="https://svelte.dev">Svelte</a>.
+    </p>
+</div>
